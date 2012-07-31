86b01be9297eda145db04b75c0b91dd91ca0f4c4
Anthony Grimes <i@raynes.me>
2012-07-31 01:26:27 -0500

    Inc version because clojars version got corrupted.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-pdo "0.1.0"
+(defproject lein-pdo "0.1.1"
   :description "Execute tasks in parallel. Like the 'do' task only... parallel."
   :url "https://github.com/Raynes/lein-pdo"
   :license {:name "Eclipse Public License"
